7a5cd825ea451be4a66539f122b0438ee881fabc
AMANQAZPLM <amanqazplm@gmail.com>
2023-09-30 10:35:48 +0530

    third comit

--- db.js
+++ db.js
@@ -7,4 +7,5 @@ export const db = mysql.createConnection({
   user: `${process.env.USER}`,
   password: `${process.env.PASSWORD}`,
   database: `${process.env.DATABASE}`,
+  port: `${process.env.DB_PORT}`,
 });
